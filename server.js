0aa4862a82766491b913fb462b864d2fa996cb58
Lulu Jovovich <lulujovovich@Lulus-MBP.lan>
2022-10-26 21:16:45 -0700

    clean up to server.js

--- server.js
+++ server.js
@@ -17,109 +17,94 @@ function loadMainPrompts() {
       choices: [
         {
           name: "View All Employees",
-          value: "VIEW_EMPLOYEES"
         },
         {
           name: "View All Employees By Department",
-          value: "VIEW_EMPLOYEES_BY_DEPARTMENT"
         },
         {
           name: "View All Employees By Manager",
-          value: "VIEW_EMPLOYEES_BY_MANAGER"
         },
         {
           name: "Add Employee",
-          value: "ADD_EMPLOYEE"
         },
         {
           name: "Remove Employee",
-          value: "REMOVE_EMPLOYEE"
         },
         {
           name: "Update Employee Role",
-          value: "UPDATE_EMPLOYEE_ROLE"
         },
         {
           name: "Update Employee Manager",
-          value: "UPDATE_EMPLOYEE_MANAGER"
         },
         {
           name: "View All Roles",
-          value: "VIEW_ROLES"
         },
         {
           name: "Add Role",
-          value: "ADD_ROLE"
         },
         {
           name: "Remove Role",
-          value: "REMOVE_ROLE"
         },
         {
           name: "View All Departments",
-          value: "VIEW_DEPARTMENTS"
         },
         {
           name: "Add Department",
-          value: "ADD_DEPARTMENT"
         },
         {
           name: "Remove Department",
-          value: "REMOVE_DEPARTMENT"
         },
         {
           name: "View Total Utilized Budget By Department",
-          value: "VIEW_UTILIZED_BUDGET_BY_DEPARTMENT"
         },
         {
           name: "Quit",
-          value: "QUIT"
         }
       ]
     }
   ]).then(res => {
     let choice = res.choice;
     switch (choice) {
-      case "VIEW_EMPLOYEES":
+      case "View All Employees":
         viewEmployees();
         break;
-      case "VIEW_EMPLOYEES_BY_DEPARTMENT":
+      case "View All Employees By Department":
         viewEmployeesByDepartment();
         break;
-      case "VIEW_EMPLOYEES_BY_MANAGER":
+      case "View All Employees By Manager":
         viewEmployeesByManager();
         break;
-      case "ADD_EMPLOYEE":
+      case "Add Employee":
         addEmployee();
         break;
-      case "REMOVE_EMPLOYEE":
+      case "Remove Employee":
         removeEmployee();
         break;
-      case "UPDATE_EMPLOYEE_ROLE":
+      case "Update Employee Role":
         updateEmployeeRole();
         break;
-      case "UPDATE_EMPLOYEE_MANAGER":
+      case "Update Employee Manager":
         updateEmployeeManager();
         break;
-      case "VIEW_DEPARTMENTS":
+      case "View All Departments":
         viewDepartments();
         break;
-      case "ADD_DEPARTMENT":
+      case "Add Department":
         addDepartment();
         break;
-      case "REMOVE_DEPARTMENT":
+      case "Remove Department":
         removeDepartment();
         break;
-      case "VIEW_UTILIZED_BUDGET_BY_DEPARTMENT":
+      case "View Total Utilized Budget By Department":
         viewUtilizedBudgetByDepartment();
         break;
-      case "VIEW_ROLES":
+      case "View All Roles":
         viewRoles();
         break;
-      case "ADD_ROLE":
+      case "Add Role":
         addRole();
         break;
-      case "REMOVE_ROLE":
+      case "Remove Role":
         removeRole();
         break;
       default:
